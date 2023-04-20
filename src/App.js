9183f6522218100065b3f96caf0625f81a01b12e
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-20 11:01:14 -0400

    hidded dashboard when not user

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>Lifting up State</h2>
-        <section style={{border:'2px solid red', width: '100%', padding:'16px'}}>
+        {!user && <><section style={{border:'2px solid red', width: '100%', padding:'16px'}}>
           <Signup setUser={setUser}/>
         </section>
         <section style={{border:'2px solid yellow', width: '100%', padding:'16px'}}>
           <Login setUser={setUser}/>
-        </section>
-        <section  style={{border:'2px solid blue', width: '100%', padding:'16px'}}>
+        </section></>}
+        {user && <section  style={{border:'2px solid blue', width: '100%', padding:'16px'}}>
           <Dashboard user={user} setUser={setUser}/>
-        </section>
+        </section>}
       </header>
     </div>
   );
